f726744423ffcdd61a43cf843459e7d113887c34
kdk <kdk1026@gmail.com>
2021-07-02 21:07:58 +0900

    readOnlyTransactionRule 수정

--- Boot2OAuthServer/src/main/java/kr/co/test/config/app/TransactionAspect.java
+++ Boot2OAuthServer/src/main/java/kr/co/test/config/app/TransactionAspect.java
@@ -48,7 +48,7 @@ public class TransactionAspect {
 	private RuleBasedTransactionAttribute readOnlyTransactionRule () {
 		RuleBasedTransactionAttribute readOnly = new RuleBasedTransactionAttribute();
 		readOnly.setReadOnly(true);
-		readOnly.setPropagationBehavior(TransactionDefinition.PROPAGATION_NOT_SUPPORTED);
+		readOnly.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
 		return readOnly;
 	}
 
